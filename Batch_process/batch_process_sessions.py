7c68f4f26cf17d049952dc52d5758fc001d3b53c
RobertoDF <roberto.defilippo@outlook.com>
2024-04-26 19:09:31 +0200

    create spikesorting mother func, update batch processing, primordial PSTH created.

---
+++ Batch_process/batch_process_sessions.py
@@ -0,0 +1,18 @@
+from Process.process import process_trials, spikesort
+from tqdm import tqdm
+from colored import Fore, Back, Style
+
+sessions = [r"/alzheimer/Roberto/Dariya/12/ephys/20231210_191835.rec/",
+            r"/alzheimer/Roberto/Dariya/12/ephys/20240126_184212.rec/",
+            r"/alzheimer/Roberto/Dariya/12/ephys/20231212_183552.rec/"]
+
+n=1
+for session in tqdm(sessions):
+    try:
+        process_trials(session)
+        spikesort(session)
+        n=+1
+    except Exception as e:
+        print(f"{Fore.white}{Back.red}{e}{Style.reset}")
+
+print(f"Processed succesfully {n} of {len(sessions)} sessions")
